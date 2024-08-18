c47916069b6f2bb2a810575ba91aa51d566855ef
Luigi Pinna <luigipinna3@gmail.com>
2024-08-18 13:46:36 +0200

    added home endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import errorHandler from "./middlewares/errorHandler";
 import requestLogger from "./middlewares/requestLogger";
@@ -11,6 +11,11 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(requestLogger);  // Logger delle requests
+
+// Home route
+app.get("/", (req: Request, res: Response) => {
+    res.send("Welcome to the API!");
+});
 
 app.use("/auth", authRoutes);
 app.use("/health", healthRoutes);
